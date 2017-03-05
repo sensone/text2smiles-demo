9dddbcaf9f271e708c94e072143d34cc6cefe9b7
Evgeni Korsunov <5345043@gmail.com>
2017-03-05 22:47:55 +0300

    refactor(clearfix): clearfix for first release

--- client/index.js
+++ client/index.js
@@ -1,32 +1,18 @@
-import React from 'react';
-import Relay from 'react-relay';
-import ReactDOM from 'react-dom';
-import useRelay from 'react-router-relay';
 import { Router, Route, browserHistory, applyRouterMiddleware } from 'react-router';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import AppLayout from './views/AppLayout';
 import EmojiForm from './views/EmojiForm';
 import NoFound from './views/NoFound';
 import './index.css';
 import './bootstrap-ubuntu.css';
 
-// Relay.injectNetworkLayer(
-//   new Relay.DefaultNetworkLayer('')
-// );
-//
-// const ViewerQueries = { viewer: () => Relay.QL`query { viewer }` };
-// (custom/:UserText/:UserBg/:UserTextBg/:TextLength)
 ReactDOM.render(
-  <Router
-    forceFetch
-    environment={Relay.Store}
-    render={applyRouterMiddleware(useRelay)}
-    history={browserHistory}
-  >
+  <Router history={browserHistory} >
     <Route component={AppLayout}>
       <Route path="/(custom/:UserText/:UserBg/:UserTextBg/:TextLength)" component={EmojiForm} />
-      <Route path="/about" component={EmojiForm} />
       <Route path="*" component={NoFound} />
     </Route>
   </Router>
-  , document.getElementById('root'), // eslint-disable-line no-undef
+  , document.getElementById('root')
 );
